11adc3681cc46c8a5a9178b187485bf0a4f4b9d8
Riley Dallas <rldaggie@gmail.com>
2016-04-14 10:27:18 -0500

    Added endpoint for /api/students

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var students = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe'
+  },
+  {
+    id: 2,
+    firstName: 'Jane',
+    lastName: 'Jenson'
+  },
+  {
+    id: 3,
+    firstName: 'Sally',
+    lastName: 'Struthers'
+  }
+]
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(students);
 });
 
 module.exports = router;
